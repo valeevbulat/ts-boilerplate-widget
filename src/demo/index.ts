a8cb2292fcb2181a73cb380c5b998527c7894c81
Valeev Bulat <cirkach95@gmail.com>
2023-01-25 01:24:49 +0300

    feat: add first widget version

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,8 +1,23 @@
 import MyLibrary from "../lib";
-const myLibraryInstance = new MyLibrary();
+const myLibraryInstance1 = new MyLibrary({ mcode: 'yourmcode' });
+const myLibraryInstance2 = new MyLibrary();
 
-document.querySelector("body").innerHTML = `<h1>Hello World!</h1>`;
+document.querySelector("body").innerHTML = `
+    <h2>Light Widget</h2>
+    <div style="padding: 100px 30px; background: #eeeeee;">
+        <div id="light-multiplier-place-1"></div>
+        <br/>
+        <div id="light-multiplier-place-2"></div>
+    </div>
+    <h2>Dark Widget</h2>
+    <div style="padding: 100px 30px; background: #0d0d0d;">
+        <div id="dark-multiplier-place-1"></div>
+        <br/>
+        <div id="dark-multiplier-place-2"></div>
+    </div>
+`;
 
-console.log("myLibraryInstance", myLibraryInstance);
-
-myLibraryInstance.myMethod(); 
+myLibraryInstance1.init({ elementSelector: '#light-multiplier-place-1', theme: 'light' });
+myLibraryInstance2.init({ elementSelector: '#light-multiplier-place-2', theme: 'light' });
+myLibraryInstance1.init({ elementSelector: '#dark-multiplier-place-1', theme: 'dark' });
+myLibraryInstance2.init({ elementSelector: '#dark-multiplier-place-2', theme: 'dark' });
